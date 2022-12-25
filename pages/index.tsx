cc50881505b18ac3d94cf223a100c4240c20342c
JC DESIGN <ismailjosim99@gmail.com>
2022-12-25 18:57:18 +0600

    add header section & config color

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,15 @@ import Categories from '../components/Home/Categories/Categories'
 import Advertisement from '../components/Home/Advertisement/Advertisement'
 import Footer from '../components/Home/Footer/Footer'
 
-
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="btn btn-outline">Button</button>
-      <button className="btn btn-outline btn-primary">Button</button>
-      <button className="btn btn-outline btn-secondary">Button</button>
-      <button className="btn btn-outline btn-accent">Button</button>
-      <Navber />
-      <Slider />
-      <Instructor />
-      <Categories />
-      <Advertisement />
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<Navber />
+			<Slider />
+			<Instructor />
+			<Categories />
+			<Advertisement />
+			<Footer />
+		</>
+	)
 }
