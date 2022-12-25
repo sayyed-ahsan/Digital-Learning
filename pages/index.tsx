4df76a43a0dddad4770003237885361bf3981f2e
sneoo <epicshakil@gmail.com>
2022-12-25 20:06:26 +0600

    brand & cupon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Navber from '../components/Home/Navber/Navbar'
 import Slider from '../components/Home/Slider/Slider'
+import Cupon from '../components/Home/Cupon/Cupon'
 import Instructor from '../components/Home/Instructor/Instructor'
 import Categories from '../components/Home/Categories/Categories'
 import Advertisement from '../components/Home/Advertisement/Advertisement'
+import Brand from '../components/Home/Brand/Brand'
 import Footer from '../components/Home/Footer/Footer'
 
 export default function Home() {
@@ -10,9 +12,11 @@ export default function Home() {
 		<>
 			<Navber />
 			<Slider />
+      <Cupon/>
 			<Instructor />
 			<Categories />
 			<Advertisement />
+      <Brand/>
 			<Footer />
 		</>
 	)
