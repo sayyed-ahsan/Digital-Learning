5767c9f9f3b24580fec8625805cc3e3a5f9c5cba
anukulghoshdev <anukulghosh264@gmail.com>
2022-12-26 13:46:27 +0600

    silder update

--- components/Home/Slider/Slider.js
+++ components/Home/Slider/Slider.js
@@ -1,34 +1,5 @@
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-
-// import img1 from '../../../public/BannerImages/1.jpg'
-// import img2 from '../../../public/BannerImages/2.jpg'
-// import img3 from '../../../public/BannerImages/3.jpg'
-
-
-// import {SliderItem} from './SliderItem'
-
-
 
 const Slider = () => {
-
-    // const bannerImageSlides = [
-    //     {
-    //         image: img1,
-    //         id: 1,
-    //         text: 'From Starters To Pro... ',
-    //     },
-    //     {
-    //         image: img2,
-    //         id: 2,
-    //         text: 'Exciting Guitar Pack',
-    //     },
-    //     {
-    //         image: img3,
-    //         id: 3,
-    //         text: 'New Jersey Guiter',
-    //     }
-    // ]
 
 
     return (
