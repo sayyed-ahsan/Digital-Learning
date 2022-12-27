bfe40788bf0e4574d502ffba4ae6ad49a677ff8a
“sayyed-ahsan” <sayyed.ahsan.dev@gmail.com>
2022-12-28 02:25:49 +0600

    footer

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Home/Navbar/Navbar';
+import Footer from "../components/Home/Footer/Footer";
+
 
 const dashboard = () => {
     return (
         <div>
             <Navbar></Navbar>
             dashboard
+            <Footer></Footer>
         </div>
     );
 };
