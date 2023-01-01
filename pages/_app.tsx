82b5849d7aa0e600e1809eb4473baf4ccaca02ba
anukulghoshdev <anukulghosh264@gmail.com>
2023-01-01 17:25:21 +0600

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar/Navbar'
-
-import { useEffect } from "react";
-import AOS from "aos";
-
-import "aos/dist/aos.css";
-// import "../scss/style.scss";
-
 import Footer from '../components/Footer/Footer';
 import { AuthProvider } from '../context/AuthProvider';
 import toast, { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
-	useEffect(()=>{
-		AOS.init();
-		AOS.refresh();
-	  }, []);
-
-	return (
-		<AuthProvider>
-			<>
-				<Navbar />
-				<Component {...pageProps} />
-				<Footer />
-				<Toaster />
-			</>
-		</AuthProvider>
-	)
+    return (
+        <AuthProvider>
+            <>
+                <Navbar />
+                <Component {...pageProps} />
+                <Footer />
+                <Toaster />
+            </>
+        </AuthProvider>
+    )
 }
