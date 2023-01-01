620d9e005ca12fcedbadab14ac7adc4300283a44
anukulghoshdev <anukulghosh264@gmail.com>
2023-01-01 17:53:09 +0600

    dashboard home and slider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,15 @@ import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer';
 import { AuthProvider } from '../context/AuthProvider';
 import toast, { Toaster } from 'react-hot-toast';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		AOS.init();
+		AOS.refresh();
+	}, []);
 	return (
 		<AuthProvider>
 			<>
