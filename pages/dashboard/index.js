a17f6570bb65473514891cc58e2c0b91d2a45ba8
“sayyed-ahsan” <sayyed.ahsan.dev@gmail.com>
2023-01-02 16:51:15 +0600

    dashboard changed

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,10 @@
 import Sidebar from "./sidebar";
 
 const Dashboard = ({ children }) => {
+
   return (
     <div className="grid grid-cols-7 gap-4">
-      <div>
+      <div className="w-[300px]">
         <Sidebar />
       </div>
       <div className="col-span-6">{children}</div>
